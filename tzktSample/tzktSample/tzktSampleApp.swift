7ce591f38477a55504aa78127aac3d458f0830b2
Requieem <43446747+Requieem@users.noreply.github.com>
2024-01-31 17:37:49 +0100

    Initial Setup

---
+++ tzktSample/tzktSample/tzktSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  tzktSampleApp.swift
+//  tzktSample
+//
+//  Created by Marco Farace on 31.01.24.
+//
+
+import SwiftUI
+
+@main
+struct tzktSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
